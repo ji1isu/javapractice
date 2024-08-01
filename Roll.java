73daf270cc61ad20387308c3eede6df16f6cf53c
Jisu,Song <songjisu777@gmail.com>
2024-08-01 22:25:16 +1000

    Added Roll

---
+++ Roll.java
@@ -0,0 +1,10 @@
+public class Roll{
+    public static void main(String[] args){
+        Random ranf = new Random();
+
+        int x = rand.nextInt(x);
+
+        System.out.println(x);
+        
+    }
+}
