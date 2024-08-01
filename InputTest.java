1f565ec01808f550a7f4e5ad5a254685df89aebf
Jisu,Song <songjisu777@gmail.com>
2024-08-01 22:14:05 +1000

    Added InputTest

---
+++ InputTest.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class InputTest{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        int number1 = scan.nextInt();
+        System.out.println(number1);
+        int number2 = scan.nextInt();
+        System.out.println(number2);
+        int number3 = scan.nextInt();
+        System.out.println(number3);
+    }
+}
