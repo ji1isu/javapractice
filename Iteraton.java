5242d0e60dc4c240e7b5a7b073f48dd4204876c4
Jisu,Song <songjisu777@gmail.com>
2024-08-04 18:09:17 +1000

    yuhhhhh

---
+++ Iteraton.java
@@ -0,0 +1,29 @@
+public class Iteration{
+    public static final String[] words = {
+        "one",
+        "two",
+        "three",
+        "four"
+    };
+    public static final int words_length = 4;
+
+    public static void forLoopVCU(){
+        //for (variable; condition; update) here!
+        for(int i = 0; i < words_length; i ++){
+            System.out.println(words[i]);
+        }
+    }
+
+    public static void forLoopIterator(){
+        //for (binding : collection) here!
+        String s = "for loop";
+        for(char c: s.toCharArray()){
+            System.out.println(c);
+        }
+    }
+
+    public static void main(String[] args){
+        forLoopVCU();
+        forLoopIterator();
+    }
+}
