ebd6d65cd88f4cef8b064b65bdab1a6d1b82fa1f
Jisu,Song <songjisu777@gmail.com>
2024-08-06 17:29:43 +1000

    planned all

---
+++ Union.java
@@ -0,0 +1,13 @@
+public class Union{
+    static int[] union(int[] x, int[] y){
+        int[] union_ls = new int[x.length + y.length];
+    }
+
+    public static void main(String[] args){
+        //you can test your code here.
+    }
+}
+
+//check in array function
+//in union; if in x[i] in array do not add to union_ls. otherwise do. same for y but the index starts at union_ls.length
+//main
