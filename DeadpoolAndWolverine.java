fae7a535182743e16d28339ca49ccded6a2afc89
Jisu,Song <songjisu777@gmail.com>
2024-08-08 11:03:24 +1000

    morning study from 9-11

---
+++ DeadpoolAndWolverine.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+import java.util.TreeMap;
+
+public class DeadpoolAndWolverine {
+
+    private double m_target_rev;
+
+    private static get_target(){
+        //get target from cli
+    }
+
+    private static String[] get_rev_and_actors(){
+        //handle input until EOF
+        //if incorrect format, print error message and continue
+    }
+
+    private static String[] get_selected_actors(){
+        //order by revenue increasing, by the target revenue
+    }
+
+
+
+    public static void main(String[] args) {
+
+        // YOUR CODE HERE
+
+    }
+
+}
+
+import java.util.TreeMap;
+
+// public class Example {
+//     public static void main(String[] args) {
+//         // Creating a TreeMap
+//         TreeMap<Integer, String> map = new TreeMap<>();
+
+//         // Adding elements
+//         map.put(3, "Three");
+//         map.put(1, "One");
+//         map.put(4, "Four");
+//         map.put(2, "Two");
+
+//         // Display the TreeMap which will be sorted by keys
+//         System.out.println("TreeMap output: " + map);
+
+//         // Access elements
+//         System.out.println("Element with key 3: " + map.get(3));
+
+//         // First and last entries
+//         System.out.println("First entry: " + map.firstEntry());
+//         System.out.println("Last entry: " + map.lastEntry());
+//     }
+// }
--- Union.java
+++ Union.java
@@ -1,13 +1,39 @@
+import java.util.*;
+
 public class Union{
+
+    private static boolean isinArray(int[] list_num, int element){
+        for(int i = 0; i < list_num.length; i++){
+            if(list_num[i] == element){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static int[] addElement(int[] src_ls, int value){
+        int[] tmp = new int[src_ls.length+1];
+        System.arraycopy(src_ls, 0, tmp, 0, src_ls.length);
+        tmp[src_ls.length] = value;
+        return tmp;
+    }
+
     static int[] union(int[] x, int[] y){
-        int[] union_ls = new int[x.length + y.length];
+        int[] union = new int[0];
+        union = x;
+        for(int i = 0; i < y.length; i++){
+            if(!isinArray(union, y[i])){
+                union = addElement(union, y[i]);
+            }
+        }
+        return union;
     }
 
     public static void main(String[] args){
-        //you can test your code here.
+        int[] x = {3, 2, 7, 2};
+        int[] y = {3, 8, 9};
+        int[] result = union(x, y);
+        System.out.println(Arrays.toString(result));//3, 2, 7, 2, 8, 9
+
     }
 }
-
-//check in array function
-//in union; if in x[i] in array do not add to union_ls. otherwise do. same for y but the index starts at union_ls.length
-//main
