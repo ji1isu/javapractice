e2c7e9cf3b44edcf8320698ab195810cbada2598
Jisu,Song <songjisu777@gmail.com>
2024-09-01 21:29:16 +1000

    Jisu Skibidi work

--- SkibidiProject/Skibidi.java
+++ SkibidiProject/Skibidi.java
@@ -1,206 +1,23 @@
-package SkibidiProject;
+public class Skibidi {
+    public static void main(String[] args) {
+        Team skibidi = new Team();
+        Team alliance = new Team();
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Set;
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.File;
+        skibidi.fileName = args[1];
+        alliance.fileName = args[0];
 
-public class Skibidi{
-    HashMap<String, Integer> allianceMap;
-    HashMap<String, Integer> skibidiMap;
-    String[] abilityList;
-    ArrayList<Integer> skibidiScoreKeeper;
-    ArrayList<Integer> allianceScoreKeeper;
-    int skibidiScore;
-    int allianceScore;
-    boolean skibidiTrapped;
-    boolean allianceTrapped;
+        skibidi.executeAll();
+        double skbdScore = skibidi.score_get();
 
-    public Skibidi(){
-        //constructors
-        allianceMap = new HashMap<>();
-        skibidiMap = new HashMap<>();
-        abilityList = new String[]{"PowerSurge", "BopShield", "EnergyBoost", "TitanBoost"};
-        skibidiScoreKeeper = new ArrayList<>();
-        allianceScoreKeeper = new ArrayList<>(); 
-        skibidiScore = 0;
-        allianceScore = 0;
-        skibidiTrapped = false;
-        allianceTrapped = false;
-    }
+        alliance.executeAll();
+        double alliScore = alliance.score_get();
 
-    private String[] parseString(String toParse){
-        String[] nowParsed = toParse.split(",\\s*|\\s+");
-        return nowParsed;
-    }
-
-    private void readFile(String fileName, HashMap<String, Integer> map, ArrayList<Integer> scoreKeeper) {
-        File file = new File(fileName);
-        if (!file.exists()) {
-            System.out.println("Error: File " + fileName + " not found. Please check the path and try again.");
-        }
-        if (file.length() == 0) {
-            System.out.println("Error: No valid data found in " + fileName + ". Battle cannot proceed.");
-        }
-        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                boolean readSuccess = sortReadLines(line, map, scoreKeeper);
-                if (!readSuccess){
-                    System.out.println("Error: Invalid parameter for '<ability>' in line '<line_content>'. Line skipped.");
-                    continue;
-                }
-            }
-        } catch (FileNotFoundException e) {
-            System.out.println("Error: File " + fileName + " not found. Please check the path and try again.");
-        } catch (IOException e) {
-            System.out.println("An I/O error occurred while reading the file.");
-        }
-    }
-
-    private void appendToScoreKeeper(HashMap<String, Integer> map, String name, ArrayList<Integer> scoreKeeper, int score){
-        if(map.containsKey(name) && !isAbility(name)){
-            scoreKeeper.add(score);
-        }
-    }
-
-    private boolean sortReadLines(String line,  HashMap<String, Integer> map, ArrayList<Integer> scoreKeeper){
-            String[] lineParsed = parseString(line);
-            if (lineParsed.length >= 2) {
-                String name = lineParsed[0]; 
-                int attackPower;
-                try {
-                    attackPower = Integer.parseInt(lineParsed[1]);
-                } catch (NumberFormatException e) {
-                    return false;
-                }
-                map.put(name, attackPower);
-                appendToScoreKeeper(map, name, scoreKeeper, attackPower);
-                return true;
-            } else {
-                return false;
-            }
-        }
-    
-
-    private boolean isAbility(String abilityName){
-        for(int i = 0; i< abilityList.length ; i++){
-            if(abilityName.equals(abilityList[i])){
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private void executeAttack(String attackName, HashMap<String, Integer> map, ArrayList<Integer> scoreKeeper){
-        switch (attackName) {
-            case "PowerSurge":
-                PowerSurge(map, scoreKeeper);
-                break;
-            case "BopShield":
-                BopShield(map, scoreKeeper);
-                break;
-            case "EnergyBoost":
-                EnergyBoost(map, scoreKeeper);
-                break;
-            case "Titanboost":
-                TitanBoost(map, scoreKeeper);
-                break;
-            case "ZapTrap":
-                // if scoreKeeper is skibidiScoreKeeper skibiditrapped is true
-                // if scorekeeper alliancescorekeeper alliancetrapped is true
-            default:
-                System.out.println("Warning: Unknown ability '<ability>' detected in '<filename>'. Ignored.");
-            }
-        }
-
-    private void PowerSurge(HashMap<String, Integer> map, ArrayList<Integer> scoreKeeper){
-        Integer value = scoreKeeper.get(0);//TODO: change multiplyBy to percentage
-        int multiplyBy = map.get("PowerSurge");
-        value = value * multiplyBy;
-        scoreKeeper.set(0, value);
-    }
-
-    private void BopShield(HashMap<String, Integer> map, ArrayList<Integer> scoreKeeper){
-        int multiplyBy = map.get("BopShield");//TODO: change multiplyBy to percentage
-        for(int i = 0; i < scoreKeeper.size(); i++){
-            Integer value = scoreKeeper.get(i);
-            value = value * multiplyBy;
-            scoreKeeper.set(i, value);
-        }
-    }
-
-    private void EnergyBoost(HashMap<String, Integer> map, ArrayList<Integer> scoreKeeper){
-        int addBy = map.get("EnergyBoost");
-        for(int i = 0; i < scoreKeeper.size(); i++){
-            Integer value = scoreKeeper.get(i);
-            value = value + addBy;
-            scoreKeeper.set(i, value);
-        }
-    }
-
-    private void ZapTrap(HashMap<String, Integer> map, int Score){
-        int minusBy = map.get("ZapTrap");
-        Score -= minusBy;
-
-    } 
-
-    private void TitanBoost(HashMap<String, Integer> map, ArrayList<Integer> scoreKeeper){
-        Integer value = scoreKeeper.get(scoreKeeper.size() - 1);
-        int multiplyBy = map.get("PowerSurge");
-        value = value * multiplyBy;
-        value = scoreKeeper.set(scoreKeeper.size() - 1, value);
-    }
-
-    private void calculateWinner(){
-        for (Integer num : skibidiScoreKeeper) {
-            skibidiScore += num;
-            if(skibidiTrapped){
-                ZapTrap(skibidiMap, skibidiScore);
-            }
-        }
-        for (Integer num : allianceScoreKeeper) {
-            allianceScore += num;
-            if(allianceTrapped){
-                ZapTrap(allianceMap, allianceScore);
-            }
-        }
-        if(skibidiScore > allianceScore){
+        if (skbdScore > alliScore){
             System.out.println("Battle result: Skibidi wins");
-        } else if(skibidiScore < allianceScore){
+        } else if (skbdScore < alliScore){
             System.out.println("Battle result: Alliance wins");
         } else{
-            System.out.println("TIE");
+            System.out.println("Battle result: Draw");
         }
-
-    }
-    public static void main(String[] args){
-        //tests
-        Skibidi skbd = new Skibidi();
-        skbd.readFile("SkibidiProject/skibidi.txt", skbd.skibidiMap, skbd.skibidiScoreKeeper);
-        System.out.println(skbd.skibidiMap);
-        System.out.println(skbd.isAbility("BopShield"));
-        System.out.println(skbd.isAbility("G-Toilet"));
-        System.out.println(skbd.skibidiScoreKeeper);
-        //main
-        skbd.readFile("SkibidiProject/skibidi.txt", skbd.skibidiMap, skbd.skibidiScoreKeeper);
-        skbd.readFile("SkibidiProject/skibidi.txt", skbd.allianceMap, skbd.allianceScoreKeeper);
-        for (String key : skbd.skibidiMap.keySet()) {
-            if(skbd.isAbility(key)){
-                skbd.executeAttack(key, skbd.skibidiMap, skbd.skibidiScoreKeeper);
-            }
-        }
-        for (String key : skbd.allianceMap.keySet()) {
-            if(skbd.isAbility(key)){
-                skbd.executeAttack(key, skbd.allianceMap, skbd.allianceScoreKeeper);
-            }
-        }
-        skbd.calculateWinner();
     }
 }
---
+++ SkibidiProject/Team.java
@@ -0,0 +1,191 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Arrays;
+import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Team {
+    private String[] abilityList = new String[]{"PowerSurge", "BopShield", "EnergyBoost", "TitanBoost", "ZapTrap"};
+    private boolean isTrapping;
+    private ArrayList<Double> characterScores;
+    private HashMap<String, Double> ability;
+    public String fileName;
+    private boolean mode;
+
+    public Team(){
+        isTrapping = false;
+        characterScores = new ArrayList<Double>();
+        ability = new HashMap<String, Double>();
+        mode = false;
+    }
+
+    private boolean isAbility(String word)
+    {
+        if (Arrays.asList(abilityList).contains(word))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    private void apply_ability(String abilityName)
+    {
+        switch (abilityName) {
+            case "PowerSurge":
+                PowerSurge();
+                break;
+            case "BopShield":
+                BopShield();
+                break;
+            case "EnergyBoost":
+                EnergyBoost();
+                break;
+            case "TitanBoost":
+                TitanBoost();
+                break;
+            case "ZapTrap":
+                isTrapping = true;
+                break;
+            default:
+                System.out.println("Warning: Unknown ability " + abilityName + " detected in " + fileName +". Ignored.");
+            }
+    }
+
+    private void PowerSurge(){
+        Double value = characterScores.get(0);
+        double multiplyBy = ability.get("PowerSurge");
+        double multiplyByPercentage = (multiplyBy * 0.01) + 1;
+        value = value * multiplyByPercentage;
+        characterScores.set(0, value);
+    }
+
+    private void BopShield(){
+        double multiplyBy = ability.get("BopShield");
+        double multiplyByPercentage = (multiplyBy * 0.01) + 1;
+        for(int i = 0; i < characterScores.size(); i++){
+            double value = characterScores.get(i);
+            value = value * multiplyByPercentage;
+            characterScores.set(i, value);
+        }
+    }
+
+    private void EnergyBoost(){
+        double addBy = ability.get("EnergyBoost");
+        for(int i = 0; i < characterScores.size(); i++){
+            double value = characterScores.get(i);
+            value = value + addBy;
+            characterScores.set(i, value);
+        }
+    }
+
+    private double ZapTrap(double Score){
+        double plusBy = ability.get("ZapTrap");
+        Score += plusBy;
+        return Score;
+    } 
+
+    private void TitanBoost(){
+        double value = characterScores.get(characterScores.size() - 1);
+        double multiplyBy = ability.get("TitanBoost");
+        value = value * multiplyBy;
+        value = characterScores.set(characterScores.size() - 1, value);
+    }
+
+
+
+    private void readFile(String fileName){
+        File file = new File(fileName);
+        if (!file.exists()) {
+            System.out.println("Error: File '" + fileName + "' not found. Please check the path and try again.");
+            System.exit(0);
+        }
+        if (file.length() == 0) {
+            System.out.println("Error: No valid data found in '" + fileName + "'. Battle cannot proceed.");
+            System.exit(0);
+        }
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                boolean readSuccess = sortReadLines(line);
+                if (!readSuccess){
+                    System.out.println("Error: Invalid parameter for '" + parseString(line)[0] + "' in line '" + line + "'. Line skipped.");
+                    continue;
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Error: File '" + fileName + "' not found. Please check the path and try again.");
+            System.exit(0);
+        } catch (IOException e) {
+            System.out.println("An I/O error occurred while reading the file.");
+            System.exit(0);
+        }
+    }
+
+    private String[] parseString(String toParse){
+        String[] nowParsed = toParse.split(",\\s*|\\s+");
+        return nowParsed;
+    }
+
+    private void appendScore(String name, Double attackPower){
+        if(!isAbility(name)){
+            characterScores.add(attackPower);
+        }
+    }
+
+    private boolean sortReadLines(String line){
+        String[] lineParsed = parseString(line);
+        if (lineParsed.length >= 2) {
+            String name = lineParsed[0]; 
+            Double attackPower;
+            try {
+                attackPower = Double.parseDouble(lineParsed[1]);
+            } catch (NumberFormatException e) {
+                return false;
+            }
+            checkAbility(name, attackPower);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private void checkAbility(String name, Double score){
+        if (isAbility(name)){
+            mode = true;
+            ability.put(name, score);
+        }
+        else
+        {
+            if(mode==true)
+            {
+                System.out.println("Warning: Unknown ability " + name + " detected in " + fileName + ". Ignored.");
+            }
+            else{
+                appendScore(name, score);
+            }
+        }
+    }
+
+    public void executeAll(){
+        readFile(fileName);
+        for(String anAbility : ability.keySet()){
+            apply_ability(anAbility);
+        }
+    }
+   
+    public double score_get()
+    {
+        double final_score = 0;
+        for (double num : characterScores) {
+            final_score += num;
+        }
+        if(isTrapping){
+            final_score = ZapTrap(final_score);
+        }
+        return final_score;
+    }
+
+}
